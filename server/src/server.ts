0ae58efb5c39288fb343b9b6c970bcec2530eb7b
Veronica <almeida.vrc@gmail.com>
2023-05-31 16:32:35 -0300

    Cria conexão com prisma e instala o cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,26 @@
-console.log('Server on');
+import Fastify from 'fastify';
+import cors from '@fastify/cors';
+import { PrismaClient } from '@prisma/client';
+
+const app = Fastify();
+const prisma = new PrismaClient();
+
+app.get('/hello', async () => {
+  const habits = await prisma.habit.findMany({
+    where: {
+      title: {
+        startsWith: 'Beber',
+      },
+    },
+  });
+
+  return habits;
+});
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP Server running on 3333!');
+  });
