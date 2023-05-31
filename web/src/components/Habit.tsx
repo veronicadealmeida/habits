5cc911219830e21745f7ed26f0f1708b33ea81fe
Veronica <almeida.vrc@gmail.com>
2023-05-31 17:48:10 -0300

    setup do tailwindcss

--- web/src/components/Habit.tsx
+++ web/src/components/Habit.tsx
@@ -3,5 +3,12 @@ interface HabitProps {
 }
 
 export function Habit(props: HabitProps) {
-  return <p>{props.completed}</p>;
+  return (
+    <div
+      className="bg-zinc-900 w-10 h-10 text-white rounded m-2 flex
+    items-center justify-center"
+    >
+      {props.completed}
+    </div>
+  );
 }
